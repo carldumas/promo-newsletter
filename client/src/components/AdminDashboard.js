a5c344323397d0d5772dc1da08d4ffa6b6ee9ec1
carldumas <dumas.carl@gmail.com>
2020-11-14 13:05:10 -0500

    Display registered email on admin dashboard page

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -1,31 +1,96 @@
 // Libraries
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { BsTrash } from 'react-icons/bs';
 // Styles
 import { Wrapper } from './Styles';
 // Configs
 import { auth } from '../firebase.js';
+import { firestore } from '../firebase';
 
 const AdminDashboard = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const unsub = firestore.collection('users').onSnapshot((snapshot) => {
+      const allUsers = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setUsers(allUsers);
+    });
+    return () => {
+      unsub();
+    };
+  }, []);
+
+  const deleteUser = (id) => {
+    firestore.collection('users').doc(id).delete();
+  };
+
   return (
-    <>
-      <Wrapper>
-        <h1>Admin Content</h1>
+    <Wrapper>
+      <div>
+        <h1>Email List</h1>
         <TextContainer>
           <hr className="horizontale-rule" />
           <p style={{ paddingTop: '20px' }}>
-            Restricted content{' '}
-            <button onClick={() => auth.signOut()}>LogOut</button>
+            You're about to send promotions newsletter to the following list:
           </p>
+          <EmailWrapper>
+            <EmailItems>
+              {users.map((user) => (
+                <Item key={user.id}>
+                  <div>
+                    <div>
+                      <div>{user.fullname}</div>
+                      <div>{user.email}</div>
+                      <div>
+                        <BsTrash
+                          style={{
+                            cursor: 'pointer',
+                          }}
+                          onClick={() => deleteUser(user.id)}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </Item>
+              ))}
+            </EmailItems>
+            <div>
+              <br />
+              <button>Send Promo Email</button>
+            </div>
+          </EmailWrapper>
         </TextContainer>
-      </Wrapper>
-    </>
+        <button onClick={() => auth.signOut()}>Admin Logout</button>
+      </div>
+    </Wrapper>
   );
 };
 
 const TextContainer = styled.div`
   margin-top: 20px;
   margin-bottom: 20px;
+`;
+
+const EmailWrapper = styled.div`
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+const EmailItems = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  padding-bottom: 10px;
+`;
+
+const Item = styled.li`
+  flex: 30%;
+  padding: 10px;
+  background: #d3d3d3;
+  margin: 5px;
 `;
 
 export default AdminDashboard;
